f8a3a8284075a0b8ee656e5f7f72653e319a807d
daankh <jarosz.dan@gmail.com>
2020-08-18 19:13:58 +0200

    add Note example

--- src/index.js
+++ src/index.js
@@ -1,39 +1,95 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
-const App = (props) => {
-  const [count, setCount] = useState(props.count);
-  const [text, setText] = useState("");
+// const App = (props) => {
+//   const [count, setCount] = useState(props.count);
+//   const [text, setText] = useState("");
 
-  const increment = () => {
-    setCount(count + 1);
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   const decrement = () => {
+//     setCount(count - 1);
+//   };
+
+//   const reset = () => {
+//     setCount(props.count);
+//   };
+
+//   return (
+//     <div>
+//       <p>
+//         The current {text || "count"} is {count}
+//       </p>
+//       <button onClick={decrement}>-1</button>
+//       <button onClick={reset}>Reset</button>
+//       <button onClick={increment}>+1</button>
+//       <input value={text} onChange={(e) => setText(e.target.value)} />
+//     </div>
+//   );
+// };
+
+const NoteApp = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addNote = (e) => {
+    e.preventDefault();
+    if (title) {
+      setNotes([...notes, { title: title.trim(), body: body.trim() }]);
+      setTitle("");
+      setBody("");
+    }
   };
 
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  const reset = () => {
-    setCount(props.count);
+  const removeNote = (id) => {
+    const newNotes = notes.filter((note, index) => index !== id);
+    setNotes(newNotes);
   };
 
   return (
     <div>
-      <p>
-        The current {text || "count"} is {count}
-      </p>
-      <button onClick={decrement}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={increment}>+1</button>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <h1>Notes</h1>
+      <div>
+        {notes.map((note, index) => (
+          <Fragment key={index}>
+            <h3>
+              <span>{note.title}</span>
+              <button onClick={() => removeNote(index)}>remove</button>
+            </h3>
+            {note.body && <p>{note.body}</p>}
+          </Fragment>
+        ))}
+      </div>
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input
+          type="text"
+          placeholder="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="body"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
     </div>
   );
 };
 
 ReactDOM.render(
   <React.StrictMode>
+    {/*
     <App count={0} />
+    */}
+    <NoteApp />
   </React.StrictMode>,
   document.getElementById("root")
 );
